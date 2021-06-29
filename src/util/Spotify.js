d7e7c527f72b5ab8ef0980612f9fc26b8cf3bee7
bornedj <bornedj@miamioh.edu>
2021-06-29 16:25:56 -0400

    able to authenticate a user with the spotify api

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,25 +1,17 @@
+import SpotifyLogin from './SpotifyKeys'
 let userAccessToken;
 const redirectUri = "http://localhost:3000/";
-const cliendId = process.env.REACT_APP_Spotify_cliendID;
-const clientSecret = process.env.REACT_APP_Spotify_clientSecret;
+const clientId = SpotifyLogin.clientID;
+
 
 const Spotify = {
-    clientId: process.env.REACT_APP_Spotify_cliendID,
-    clientSecret: process.env.REACT_APP_Spotify_clientSecret,
-    redirectUri: "http://localhost:3000/",
-    
     getAccessToken() {
         //check if there is already a userAccessToken if not need to acquire one
         if (userAccessToken) {
             return userAccessToken;
         } else {
-
-            //just testing right now
-            console.log(process.env.REACT_APP_Spotify_cliendID);
-            return;
             //sending users to authenticate their spotify account
-            // window.location = `https://accounts.spotify.com/authorize?client_id=${this.clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
-            window.location = "https://accounts.spotify.com/authorize?client_id=" + this.cliendId + "&response_type=token&scope=playlist-modify-public&redirect_uri=" + this.redirectUri; 
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
 
             const userAccessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
             const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
@@ -28,6 +20,8 @@ const Spotify = {
                 userAccessToken = userAccessTokenMatch;
                 const expiresIn = Number(expiresInMatch);
 
+                console.log(userAccessToken)
+                console.log(expiresIn)
                 //clearing the parameters
                 window.setTimeout(() => userAccessToken = '', expiresIn * 1000);
                 window.history.pushState('Access token', null, '/')
